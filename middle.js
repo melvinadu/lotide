e0b4bba6e7463f8627d624d8a08506fffe7b5b67
Melvin Adu <contactmelvinadu@gmail.com>
2021-11-29 19:54:48 +0000

    dried up middle.js and created an test file

--- middle.js
+++ middle.js
@@ -1,49 +1,8 @@
-const eqArrays = function(firstArray, secondArray) {
-  
-  let longerArrayLength = 0;
-  
-  if (firstArray.length > secondArray.length) {
-    longerArrayLength = firstArray.length;
-  } else {
-    longerArrayLength = secondArray.length;
-  }
-  
-  for (let i = 0; i < longerArrayLength; i++) {
-    
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function(firstArray, secondArray) {
-    
-  if (firstArray.length !== secondArray.length) {
-    console.log("The first array is NOT equal to the second array.");
-    return false;
-  } 
-  
-  for (let i = 0; i < firstArray.length; i++) {
-    
-    if (firstArray[i] !== secondArray[i]) {
-      console.log("The first array is NOT equal to the second array.");
-      return false;
-    } 
-
-  }
-  
-  console.log("The first array IS equal to the second array.");
-  return true;
-  
-}
-
 // FUNCTION IMPLEMENTATION
 
-let newArray = [];
-
 const middle = function(array) {
-  
+  let newArray = [];
+
   if (array.length < 3) {
 
     console.log(newArray);
@@ -74,12 +33,4 @@ const middle = function(array) {
   } 
 }
 
-//TEST CODE
-middle([1]) // => []
-middle([1, 2]) // => []
-
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+module.exports = middle;
